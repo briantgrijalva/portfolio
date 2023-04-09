a5fd34af9aa5ca2fb97306a887c004006f0ba8af
Briant Grijalva <briantgrijalva20@gmail.com>
2023-04-09 13:05:17 -0600

    app folder removed

--- app/font.ts
+++
@@ -1,8 +0,0 @@
-import { Montserrat as customFont } from 'next/font/google'
-
-export const font = customFont({
-  weight: ['400', '700'],
-  variable: '--custom-font',
-  subsets: ['latin']
-  // preload: false
-})
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.css'
+import { font } from './font'
 import RootLayout from "./layout";
 
 export default function MyApp({ Component, pageProps }:any) {

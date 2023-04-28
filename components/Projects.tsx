fa6c30fe10ee68111fc483983a342c9ec764e99c
Briant Grijalva <briantgrijalva20@gmail.com>
2023-04-28 17:52:41 -0600

    padding fix

---
+++ components/Projects.tsx
@@ -0,0 +1,25 @@
+import PaperShipX from '../assets/img/imgPapershipx.png'
+import inventory from '../assets/img/inventory-app.png'
+import todo from '../assets/img/todolist-skill.png'
+import ProjectCard from '@/components/ProjectCard'
+export default function Projects () {
+    return (
+      <div className='container'>  
+      <div className="row justify-content-center pt-5 mt-5">
+        <div className="col-md-12 col-sm-12 col-lg-6 text-center">
+          <h2>A few projects that I&apos;ve worked on</h2>
+        </div>
+      </div>
+    
+      <div className='mt-5 py-5 mb-5'>
+        <ProjectCard name='PaperShipX' description="An android videogame I've created, It has a paper inspired design, people who have played it say it's too hard." technologies={['Unity', 'C#', 'Illustrator', 'Videogame design']} reverse={false} image={PaperShipX} />
+      </div>
+      <div className='mt-5 py-5 mb-5'>
+        <ProjectCard name='To-Do App' description='A minimal To-Do List App.' technologies={['React', 'Node JS', 'MongoDB', 'CSS', 'Bootstrap', 'HTML', 'TypeScript']} reverse={true} image={todo} />
+      </div>
+      <div className='mt-5 py-5 mb-5'>
+        <ProjectCard name='Inventory App' description='An Inventory App that was created by a friend and me just for fun.' technologies={['React', 'Node JS', 'MongoDB', 'CSS', 'Bootstrap', 'HTML']} reverse={false} image={inventory} />
+      </div>
+    </div>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,12 @@
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import briantGrijalva from '../assets/img/briant-grijalva.jpg'
-import PaperShipX from '../assets/img/imgPapershipx.png'
-import inventory from '../assets/img/inventory-app.png'
-import todo from '../assets/img/todolist-skill.png'
-import ProjectCard from '@/components/ProjectCard'
 import Skills from '@/components/Skills'
 import Contact from '@/components/Contact'
 import { font } from '@/assets/fonts/font'
 import Head from 'next/head'
 import Services from '@/components/Services'
+import Projects from '@/components/Projects'
 
 export default function HomePage () {
   return (
@@ -51,20 +48,7 @@ export default function HomePage () {
           <Services />
         </section>
         <section className='mt-5 py-5 mb-5'>
-          <div className="row justify-content-center pt-5 mt-5">
-            <div className="col-md-12 col-sm-12 col-lg-6 text-center">
-              <h2>A few projects that I&apos;ve worked on</h2>
-            </div>
-          </div>
-          <div className='mt-5 py-5 mb-5'>
-            <ProjectCard name='PaperShipX' description="An android videogame I've created, It has a paper inspired design, people who have played it say it's too hard." technologies={['Unity', 'C#', 'Illustrator', 'Videogame design']} reverse={false} image={PaperShipX} />
-          </div>
-          <div className='mt-5 py-5 mb-5'>
-            <ProjectCard name='To-Do App' description='A minimal To-Do List App.' technologies={['React', 'Node JS', 'MongoDB', 'CSS', 'Bootstrap', 'HTML', 'TypeScript']} reverse={true} image={todo} />
-          </div>
-          <div className='mt-5 py-5 mb-5'>
-            <ProjectCard name='Inventory App' description='An Inventory App that was created by a friend and me just for fun.' technologies={['React', 'Node JS', 'MongoDB', 'CSS', 'Bootstrap', 'HTML']} reverse={false} image={inventory} />
-          </div>
+          <Projects />
         </section>
         <section id='skills' className='mt-5 pt-5 mb-5 pb-5'>
           <Skills />

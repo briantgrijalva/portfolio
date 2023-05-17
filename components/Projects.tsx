348f57b6beafe83ac13df2801533cce0f6a49d38
Briant Grijalva <briantgrijalva20@gmail.com>
2023-05-16 18:33:44 -0600

    Update services

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -7,22 +7,22 @@ import { FormattedMessage } from 'react-intl'
 export default function Projects () {
     return (
       <div className='container'>  
-      <div className="row justify-content-center pt-5 mt-5">
+      <div className="row justify-content-center mt-5">
         <div className="col-md-12 col-sm-12 col-lg-6 text-center">
           <h2><FormattedMessage id="page.home.projects.title" /></h2>
         </div>
       </div>
     
-      <div className='mt-5 py-5 mb-5'>
+      <div className='mt-5 py-4 mb-5'>
         <ProjectCard name='Pomo Mode' description={<FormattedMessage id="page.home.projects.first" />} technologies={['React Native', 'Styled components', 'UI/UX', 'TypeScript']} reverse={false} image={PomoMode} url='https://play.google.com/store/apps/details?id=com.briantgrijalva.PomoMode'/>
       </div>
-      <div className='mt-5 py-5 mb-5'>
+      <div className='mt-5 py-4 mb-5'>
         <ProjectCard name='PaperShipX' description={<FormattedMessage id="page.home.projects.second" />} technologies={['Unity', 'C#', 'Illustrator', 'Videogame design']} reverse={false} image={PaperShipX} url='https://play.google.com/store/apps/details?id=com.KirinXstudio.PaperShipX'/>
       </div>
-      <div className='mt-5 py-5 mb-5'>
+      <div className='mt-5 py-4 mb-5'>
         <ProjectCard name='To-Do App' description={<FormattedMessage id="page.home.projects.third" />} technologies={['React', 'Node JS', 'MongoDB', 'CSS', 'Bootstrap', 'HTML', 'TypeScript', 'UI/UX']} reverse={false} image={todo} url='https://skill-todo-list.netlify.app/' />
       </div>
-      <div className='mt-5 py-5 mb-5'>
+      <div className='mt-5 py-4 mb-5'>
         <ProjectCard name='Inventory App' description={<FormattedMessage id="page.home.projects.fourth" />} technologies={['React', 'Node JS', 'MongoDB', 'CSS', 'Bootstrap', 'HTML', 'UI/UX']} reverse={false} image={inventory} url='https://inventario-mb.netlify.app/' />
       </div>
     </div>

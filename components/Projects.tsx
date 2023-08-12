45fe8c515a262c8145340a2b769b86b31b9b5b9e
Briant Grijalva <briantgrijalva20@gmail.com>
2023-08-12 16:17:12 -0600

    Remove some projects

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -32,12 +32,6 @@ export default function Projects () {
       <div className='mt-5 py-4 mb-5'>
         <ProjectCard name='PaperShipX' description={<FormattedMessage id="page.home.projects.second" />} technologies={['Unity', 'C#', 'Illustrator', 'Videogame design']} reverse={false} image={PaperShipX} url='https://play.google.com/store/apps/details?id=com.KirinXstudio.PaperShipX'/>
       </div>
-      <div className='mt-5 py-4 mb-5'>
-        <ProjectCard name='To-Do App' description={<FormattedMessage id="page.home.projects.third" />} technologies={['React', 'Node JS', 'MongoDB', 'CSS', 'Bootstrap', 'HTML', 'TypeScript', 'UI/UX']} reverse={false} image={todo} url='https://skill-todo-list.netlify.app/' />
-      </div>
-      <div className='mt-5 py-4 mb-5'>
-        <ProjectCard name='Inventory App' description={<FormattedMessage id="page.home.projects.fourth" />} technologies={['React', 'Node JS', 'MongoDB', 'CSS', 'Bootstrap', 'HTML', 'UI/UX']} reverse={false} image={inventory} url='https://inventario-mb.netlify.app/' />
-      </div>
     </div>
     )
 }
